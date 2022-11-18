b343dd8a93929c95bb445b8fc2ddb229c8f70c1a
shanku45 <shankunatra678@gmail.com>
2022-11-18 12:30:39 +0530

    first commit

---
+++ src/main/java/csesonar/sonarcse/App.java
@@ -0,0 +1,13 @@
+package csesonar.sonarcse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
